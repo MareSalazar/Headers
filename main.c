b7f58fa4f6cf333b5bd38ea5afdcd017c2de629b
Laboratorio <laboratorio@udla-ec.int>
2026-05-07 15:06:27 -0500

    Actualice archivos

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include "sumar.h"
+#include "restar.h"
+#include "multiplicacion.h"
+#include "dividir.h"
 
 int main()
 {
@@ -8,7 +11,10 @@ int main()
     scanf("%d",&a);
     printf("Ingrese 2do número:");
     scanf("%d",&b);
-    printf("%d", sumar(a,b));
+    printf("El resultado de la suma es: %d", sumar(a,b));
+    printf("El resultado de la resta es:%d", restar(a,b));
+    printf("El resultado de la multiplicacion es:%d", multiplicacion(a,b));
+    printf("El resultado de la division es:%d", dividir(a,b));
     return 0;
 
 }
